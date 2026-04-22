59f6912e6db5691093fb5c870a95b2b5d2eea546
Brian Henning <henninb@msn.com>
2026-04-22 05:36:39 -0500

    wip

---
+++ src/components/SiteCard.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps({
+  site: Object
+})
+</script>
+
+<template>
+  <a :href="site.url" class="card">
+    <span class="card-name">{{ site.name }}</span>
+    <span class="card-path">{{ site.path }}</span>
+    <span class="arrow">↗</span>
+  </a>
+</template>
